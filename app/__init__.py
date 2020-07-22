981af235637d5ed7f9fcc6384803dfb00b679b70
Angelica <Angelica137@users.noreply.github.com>
2020-07-22 10:50:20 +0100

    create home page

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,4 @@
 from flask import Flask
-from app import routes
 
 app = Flask(__name__)
-
-@app.route("/")
-@app.route("/index")
-def index():
-	return "<h1>Hello World!<h1>"
+from app import routes
