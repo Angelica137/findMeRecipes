34735eca694a5fc1eb4fc4de72edd32b88181be8
Angelica <Angelica137@users.noreply.github.com>
2020-07-29 23:26:40 +0100

    create user model

---
+++ app/models.py
@@ -0,0 +1,16 @@
+import flask
+from app import db
+frpm werkzeug.security import generate_apssword_hash, check_password_hash
+
+
+class User(db.Document):
+    user_id = db.IntField(unique=True)
+    first_name = db.StringField(max_length=50)
+    email = db.StringField(max_length=30, unique=True)
+    password = db.StringField()
+
+    def set_password(self, password):
+        self.password = generate_password_hash(self.password, password)
+
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
